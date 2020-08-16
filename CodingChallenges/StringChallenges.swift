638b8ced659d013d8ab76a917ed3f7fe13b79d3b
OddMagnet <mibruenen@gmail.com>
2020-08-16 10:03:27 +0200

    Add string challenge 2

--- CodingChallenges/StringChallenges.swift
+++ CodingChallenges/StringChallenges.swift
@@ -9,8 +9,14 @@
 import Foundation
 
 struct StringChallenges {
+    // Challenge 1: Are the letters unique?
     func challenge1(input: String) -> Bool {
         let charSet = Set(input)
         return input.count == charSet.count
+    }
+
+    // Challenge 2: Is a string a palindrome?
+    func challenge2(input: String) -> Bool {
+        return input.lowercased() == String(input.reversed()).lowercased()
     }
 }
--- CodingChallengesTests/CodingChallengesTests.swift
+++ CodingChallengesTests/CodingChallengesTests.swift
@@ -20,4 +20,19 @@ class CodingChallengesTests: XCTestCase {
         XCTAssert(str.challenge1(input: "Hello, world") == false, "Challenge 1 failed")
     }
 
+    func testChallenge2() {
+        XCTAssert(str.challenge2(input: "rotator") == true, "Challenge 2 failed")
+        XCTAssert(str.challenge2(input: "Rats live on no evil star") == true, "Challenge 2 failed")
+        XCTAssert(str.challenge2(input: "Never odd or even") == false, "Challenge 2 failed")
+        XCTAssert(str.challenge2(input: "Hello, world") == false, "Challenge 2 failed")
+
+        /*
+         • The string “rotator” should return true.
+         • The string “Rats live on no evil star” should return true.
+         • The string “Never odd or even” should return false; even though the letters are the same in
+         reverse the spaces are in different places.
+         • The string “Hello, world” should return false because it reads “dlrow ,olleH” backwards.
+         */
+    }
+
 }
